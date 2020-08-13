2298d20bef9261cd6a20d1ff523f2993e0583237
Jon Sullivan <JSullivan@OrangeBarrelMedia.com>
2020-08-13 18:58:54 -0400

    init

---
+++ index.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react'
+
+const NetlifyForm = ({
+  formName,
+  preSubmit,
+  postSubmit,
+  formValues,
+  children,
+}) => {
+  /* 
+
+  - Automatic Honeypot ✅
+  - Pull this form in and have a functioning, awesome Netlify form. Literally
+    no other plumbing required.
+
+  API: 
+    formName
+    formValues
+    
+  Optional:
+    preSubmit (async ready, return true, else no-op)
+    postSubmit (no return necessary)
+    automaticHoneypot
+
+  */
+
+  // Complicated stuff to keep things well-hidden during runtime (and from bots)
+  // but exposed for netlify build bots
+  const [inNetlifyBuild, setInNetlifyBuild] = useState(true)
+  useEffect(() => {
+    setInNetlifyBuild(false)
+  }, [])
+
+  // Transform object to proper form data
+  const formEncodeString = (str) => encodeURIComponent(str).replace(/%20/g, '+')
+  const encodeData = (obj) => (
+    Object.entries(obj)
+      .map(pair =>
+        formEncodeString(pair[0])
+        + '='
+        + formEncodeString(pair[1])
+      )
+      .join('&')
+  )
+
+  // Submit via POST then pass back true
+  const handleSubmit = async () => {
+    return (
+      await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encodeData({
+          ...formValues,
+          "form-name": formName,
+          infoo: honey
+        })
+      })
+    )
+  }
+
+  // Wrapper for pre, submit, post
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    if ((preSubmit && await preSubmit()) || !preSubmit) {
+      if (await handleSubmit()) {
+        postSubmit && postSubmit()
+      }
+      else {
+        // Figure out how to handle this better
+        // It shouldn't happen, but you never know
+        throw new Error("Error submitting to Netlify")
+      }
+    }
+    else {
+      console.log('preSubmit false')
+    }
+  }
+
+  // Honeypot Input state
+  const [honey, setHoney] = useState('')
+
+  return (
+    inNetlifyBuild
+      ? <form name={formName} onSubmit={onSubmit} data-netlify="true" netlify-honeypot="infoo">
+        {children}
+      </form>
+      : <form onSubmit={onSubmit}>
+        {children}
+        <p style={{ opacity: '0', position: 'absolute', top: '0', left: '0' }}>
+          <input style={{ width: '0', height: '0', zIndex: '-1' }} name="description" value={honey} onChange={(e) => setHoney(e.target.value)} />
+        </p>
+      </form>
+  )
+}
+
+export default NetlifyForm
